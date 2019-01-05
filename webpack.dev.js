e039dd6e2aeaed038db338a4181724a6af391867
Udesh Kumarasinghe <mail@udeshuk.me>
2019-01-05 20:53:15 +0530

    Initialized webpack with material design

---
+++ webpack.dev.js
@@ -0,0 +1,43 @@
+const autoprefixer = require('autoprefixer');
+const path = require('path');
+
+module.exports = {
+    devServer: {
+        contentBase: path.join(__dirname, 'src'),
+        compress: true,
+        port: 9000
+    },
+    entry: ['./src/scss/main.scss', './src/js/app.js'],
+    output: {
+        filename: '[name].min.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /.scss$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].min.css',
+                        },
+                    },
+                    {loader: 'extract-loader'},
+                    {loader: 'css-loader'},
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: () => [autoprefixer()],
+                        },
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            includePaths: ['./node_modules'],
+                        },
+                    }],
+            }
+        ],
+    },
+};
