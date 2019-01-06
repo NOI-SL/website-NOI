37417300d59461af8762cc0ce513f1ff76314e19
Udesh Kumarasinghe <mail@udeshuk.me>
2019-01-06 16:12:35 +0530

    Implemented gallery

---
+++ src/components/GalleryCard.vue
@@ -0,0 +1,16 @@
+<template>
+  <li class="mdc-image-list__item">
+    <img class="mdc-image-list__image" :src="item.src">
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    item: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
