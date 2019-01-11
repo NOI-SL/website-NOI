644ae37f9d8de78a2c39c71ecbbd3973e6214a26
NishanWijethunga <nishansameerawijethunga@gmail.com>
2019-01-11 14:18:04 +0530

    contact page created

--- src/components/contact/Contact.vue
+++ src/components/contact/Contact.vue
@@ -1,6 +1,14 @@
 <template>
-  <div class="container main-section about">
-    <h1>Contact</h1>
+  <div class="container-flud main-section about" id="page-contact">
+    <section id="section-1">
+      <div class="back"></div>
+      <div class="content">
+        <div class="form-content">
+          <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSc4g_KkEX85LjSxHoJXLuyzBR729XomVAQMOWrKJYeDl3xZWg/viewform?embedded=true" width="640" height="949" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
+
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
