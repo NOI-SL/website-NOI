50bec7d8e18ef3e9d1da38023035ee2972baf0de
NishanWijethunga <nishansameerawijethunga@gmail.com>
2019-01-12 14:39:41 +0530

    About page created

--- src/components/about/About.vue
+++ src/components/about/About.vue
@@ -1,6 +1,111 @@
 <template>
-  <div class="container main-section about">
-    <h1>About</h1>
+  <div class="container-flud main-section about" id="page-about">
+    <section id="section-1">
+      <div class="back"></div>
+      <div class="content">
+        <div class="content-inner">
+          <div class="organizers">
+            <div class="org-container">
+              <div class="mdc-card demo-card demo-card--hero">
+                <div class="mdc-card__primary-action mdc-ripple-upgraded" tabindex="0">
+                  <div class="mdc-card__media mdc-card__media--16-9 demo-card__media" style="background-image: url('../../img/acm_ucsc.png');"></div>
+                  <div class="demo-card__primary">
+                    <h2 class="demo-card__title mdc-typography--headline6">Organized by</h2>
+                    <!-- <h3 class="demo-card__subtitle mdc-typography--subtitle2">by Codearchery</h3> -->
+                  </div>
+                  <div class="demo-card__secondary mdc-typography--body2">
+                    <p class="text">National Olympiad in Informatics program is organized by the ACM student Chapter of UCSC, with collaboration of IOI alumni of Sri Lanka.</p>
+                  </div>
+                </div>
+                <!-- <div class="mdc-card__actions">
+                  <div class="mdc-card__action-buttons">
+                    <a target="_blank" href=""><button class="mdc-button mdc-button--raised mdc-ripple-upgraded">Website</button></a>
+                  </div>
+                </div> -->
+              </div>
+            </div>
+          </div>
+          <div class="sponsors">
+            <div class="sp-container">
+              <div class="mdc-card demo-card demo-card--hero">
+                <div class="mdc-card__primary-action mdc-ripple-upgraded" tabindex="0">
+                  <div class="mdc-card__media mdc-card__media--16-9 demo-card__media" style="background-image: url('../../img/oDoc-logo.png');"></div>
+                  <div class="demo-card__primary">
+                    <h2 class="demo-card__title mdc-typography--headline6">Sponsored by</h2>
+                    <!-- <h3 class="demo-card__subtitle mdc-typography--subtitle2">by Codearchery</h3> -->
+                  </div>
+                  <div class="demo-card__secondary mdc-typography--body2">
+                    <p class="text">oDoc is the sole sponsor for IOI 2019 program.</p>
+                  </div>
+                </div>
+                <!-- <div class="mdc-card__actions">
+                  <div class="mdc-card__action-buttons">
+                    <a target="_blank" href=""><button class="mdc-button mdc-button--raised mdc-ripple-upgraded">Website</button></a>
+                  </div>
+                </div> -->
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section id="section-2">
+      <div class="back"></div>
+      <div class="content">
+        <div class="content-inner">
+          <div class="logo-content">
+            <div class="logo">
+              <img class="img-responsive" src="../../img/UCSC_logo.png" alt="">
+            </div>
+          </div>
+          <div class="desc">
+            <div class="desc-inner">
+              <p class="desc-text"><span class="material-icons">code</span><span>The University of Colombo School of Computing (UCSC) is the Higher Educational Institute affiliated to the University of Colombo providing Computer Science and Information and Communication Technology education. </span></p>
+              <p class="desc-text"><span class="material-icons">code</span><span>In striving for excellence in education, UCSC has always attempted to foresee the needs of the country as a whole.</span></p>
+              <p class="desc-text"><span class="material-icons">code</span><span>Introducing Computer Science as a field of specialization as early as in 1985, UCSC produced Software Engineers for the emerging software industry in Sri Lanka in addition to making fit the best of these for research and higher education. </span></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section id="section-3">
+      <div class="back"></div>
+      <div class="content">
+        <div class="content-inner">
+          <div class="logo-container">
+            <div class="logo">
+              <img class="img-responsive" src="../../img/oDoc-logo.png" alt="">
+            </div>
+          </div>
+          <div class="video-container">
+            <div class="video">
+              <div class="mdc-card demo-card demo-card--hero">
+                <div class="mdc-card__primary-action mdc-ripple-upgraded" tabindex="0">
+                  <div class="mdc-card__media mdc-card__media--16-9 demo-card__media">
+                    <iframe width="100%" height="315" src="https://www.youtube.com/embed/uTk26ULYefc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                  </div>
+                  <!-- <div class="demo-card__primary">
+                    <h2 class="demo-card__title mdc-typography--headline6">C++ Animated Training Videos</h2>
+                    <h3 class="demo-card__subtitle mdc-typography--subtitle2">by Codearchery</h3>
+                  </div> -->
+                  <!-- <div class="demo-card__secondary mdc-typography--body2">
+                    This Video series is for those who want to easily understand all the concept of C++ and those who hates to watch only a Screenshot Video. I've used White Board Animation to Explain each concept Innovatively.
+                  </div> -->
+                </div>
+                <!-- <div class="mdc-card__actions">
+                  <div class="mdc-card__action-buttons">
+                    <a target="_blank" href="https://www.youtube.com/watch?v=uTk26ULYefc"><button class="mdc-button mdc-button--raised mdc-ripple-upgraded">youtube link</button></a>
+                  </div>
+                </div> -->
+              </div>
+            </div>
+          </div>
+        </div>
+        <p class="desc-text"><span class="material-icons">code</span><span>oDoc is the sole sponsor for IOI 2019 program.</span></p>
+      </div>
+    </section>
   </div>
 </template>
 
