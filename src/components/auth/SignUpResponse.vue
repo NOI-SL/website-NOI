e496cc9d355427141bee6bbe8f439cd6d24dabfb
Pasindu Wijesena <pasindu.wijesena@gmail.com>
2019-02-03 02:03:47 +0530

    - signup confirmation page updates

--- src/components/auth/SignUpResponse.vue
+++ src/components/auth/SignUpResponse.vue
@@ -4,10 +4,14 @@
             <div class="content">
                 <div class="container">
                     <div class="mdc-elevation--z2 response-container">
-                        <p class="heading-success">You are successfully signed up.</p>
+                        <p class="heading-success">You have successfully signed up for the NOI Program</p>
                         <div class="content-message">
-                            <p class="thank-you">Thank you !</p>
-                            <p class="text">NOI Portal credentials have been mailed to your email address.</p>
+                            <p class="thank-you">Thank you!</p>
+                            <p class="text">
+                                NOI Portal login credentials will be mailed to your email address.<br>
+                                Keep in touch with your mail inbox, we will send updates on the program to your inbox.<br>
+                                Don't hesitate to send your inquiries through the contact form.
+                            </p>
                         </div>
                     </div>
                 </div>
